1297fdec9352d6a0d89122da1fd183bbb52f5a87
黄海锋 <emlia@hhf.local>
2019-08-05 08:34:52 +0800

    modify popup

--- src/components/popup/index.vue
+++ src/components/popup/index.vue
@@ -1,8 +1,6 @@
 <template>
-  <div v-if="show" class="wrapper" @click="$emit('mask_click')">
-    <transition name="slide-fade">
-      <div :style="content_style" class="content"></div>
-    </transition>
+  <div v-if="anim_show" class="wrapper" @click="$emit('mask_click')">
+    <div :style="content_style" class="content" :class="content_class"></div>
   </div>
 </template>
 <script>
@@ -27,20 +25,40 @@ export default {
   },
   components: {},
   data() {
-    return {};
-  },
-  computed: {
-    content_style() {
-      return {
-        width: this.width,
-        top: 0,
-        bottom: 0,
-        left: 0,
+    return {
+      content_style: {
         backgroundColor: this.bgColor
-      };
+      },
+      content_class: [],
+      anim_show: false
+    };
+  },
+  created() {},
+  computed: {},
+  methods: {
+    start_anim() {
+      this.content_class = ["init"];
+      this.anim_show = true;
+      setTimeout(() => {
+        this.content_class = ["final"];
+      }, 100);
+    },
+    fade_anim() {
+      this.content_class = ["init"];
+      setTimeout(() => {
+        this.anim_show = false;
+      }, 800);
     }
   },
-  methods: {}
+  watch: {
+    show(value) {
+      if (value) {
+        this.start_anim();
+      } else {
+        this.fade_anim();
+      }
+    }
+  }
 };
 </script>
 <style scoped>
@@ -52,7 +70,17 @@ export default {
   left: 0;
   right: 0;
 }
+.init {
+  width: 0;
+}
+.final {
+  width: 200px;
+}
 .content {
+  top: 0;
+  bottom: 0;
+  left: 0;
   position: absolute;
+  transition: all 800ms ease;
 }
 </style>
